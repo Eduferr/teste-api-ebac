2462c893724625d2c8c84ab70e3b45fe6a75c858
Eduferr <eduferr2803@gmail.com>
2025-11-05 23:22:06 -0300

    Implementa mochawesome-report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+
   e2e: {
+    baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:'http://localhost:3000/'
+
   },
 });
